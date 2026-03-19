5048f40829de66a1bcafbc64acca1a0c757e386f
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2026-03-18 18:40:50 -0700

    testing something
    This is going to take so long to build

--- config/config-example.js
+++ config/config-example.js
@@ -19,7 +19,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: '34.125.114.43',
+	host: 'sim.thetrainercorner.net',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
